e16b6422f58aa3021777992cefdc8852cf93424f
jemel <jemel.defreitas@gmail.com>
2023-02-26 02:24:18 -0600

    commit tree 91528820e67772c9c0539da5ff59052fc80090da

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+let node = document.getElementById("main");
+if (node.parentNode) 
+  node.parentNode.removeChild(node);
+
+const newHeader = document.createElement("H1");
+
+newHeader.id = "victory";
+
+  const newContent = document.createTextNode("JEMEL is the champion");
+
+  newHeader.appendChild(newContent);
+
+
+
